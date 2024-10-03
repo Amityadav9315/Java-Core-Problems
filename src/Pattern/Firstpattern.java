cab70f7b5391560d8ae5f792ebaf6b57f1806a44
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-03 11:14:04 +0530

    FirstPattern

---
+++ src/Pattern/Firstpattern.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+public class Firstpattern {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0; i<n; i++) {
+			for(int j=1; j<n; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/Pattern/SecondPattern.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class SecondPattern {
+	public class Firstpattern {
+		public static void main(String[] args) {
+			int n=5;
+			for(int i=0; i<n; i++) {
+				for(int j=1; j<n; j++) {
+					System.out.print(i+" ");
+					System.out.println("Ram");
+				}
+				System.out.println();
+			}
+
+}
+	}
+}
