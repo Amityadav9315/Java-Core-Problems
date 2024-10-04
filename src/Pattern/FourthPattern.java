2aa57adaf5ecbbb8eb4f4147af179d20a58ae0a7
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-04 19:40:03 +0530

    FourthPattern

---
+++ src/Pattern/FourthPattern.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+
+
+public class FourthPattern {
+	public static void main(String[] args) {
+		int n=5;
+		int count=0;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				count++;
+				System.out.print(count);
+				
+				
+			}
+			
+			
+			System.out.println();
+		}
+		
+		
+	}
+
+}
