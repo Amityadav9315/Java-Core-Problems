432c52df437975f0c46dedc0f2f7cdc27d581038
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-10 09:37:51 +0530

    Rectangle

---
+++ src/Oops/Rectangle.java
@@ -0,0 +1,58 @@
+package Oops;
+
+public class Rectangle {
+	 
+	private double length; 
+	private double breadth;
+	public String getLength;
+	public String getBreadth;
+	public double getLength() {
+		return length;
+	}
+	public double getBreadth() {
+		return breadth;
+	}
+	public void setLength(double l) {
+	    length=l;
+	}
+	public void setBreadth(double b) {
+	 breadth=b;
+	}
+	public double area() {
+		return length*breadth;
+	}
+	public double perimeter() {
+		return 2*(length+breadth);
+	}
+	public boolean isSquare() {
+		if(length==breadth) {
+			return true;
+		}
+			else {
+				return false;
+			}
+		}
+	//public class RectangleTest() {
+	
+		
+	
+	
+	
+	
+//}
+	public static void main(String[] args) {
+		Rectangle r=new Rectangle();
+		r.length=10.5;
+		r.breadth=10.5;
+		System.out.println("Area "+r.area());
+		System.out.println("Perimeter "+r.perimeter());
+		System.out.println("It is square "+r.isSquare());
+		System.out.println("Length "+r.getLength());
+		System.out.println("Breadth"+r.getBreadth());
+		
+		
+		
+		
+	}
+
+}
